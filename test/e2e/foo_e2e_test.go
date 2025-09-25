ae19e554f02d3cac077db3cd4e531ad7004f5b29
Rahul Mishra <rahul.iert09@gmail.com>
2025-09-25 20:24:18 +0530

    Update foo_e2e_test.go

--- test/e2e/foo_e2e_test.go
+++ test/e2e/foo_e2e_test.go
@@ -9,13 +9,13 @@ import (
 	. "github.com/onsi/gomega"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
-
-	unstructuredv1 "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/dynamic"
 )
 
 var kubeClient *kubernetes.Clientset
@@ -39,9 +39,12 @@ var _ = BeforeSuite(func() {
 		Resource: "foos",
 	}
 
+	// Try in-cluster first, then kubeconfig
 	cfg, err := rest.InClusterConfig()
 	if err != nil {
-		cfg, err = clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+		loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+		configOverrides := &clientcmd.ConfigOverrides{}
+		cfg, err = clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides).ClientConfig()
 	}
 	Expect(err).NotTo(HaveOccurred())
 
@@ -57,7 +60,7 @@ var _ = BeforeSuite(func() {
 // -------------------
 var _ = Describe("Foo Controller", func() {
 	It("should create a Deployment when Foo is created", func() {
-		foo := &unstructuredv1.Unstructured{
+		foo := &unstructured.Unstructured{
 			Object: map[string]interface{}{
 				"apiVersion": "example.com/v1",
 				"kind":       "Foo",
@@ -74,9 +77,13 @@ var _ = Describe("Foo Controller", func() {
 		_, err := dynClient.Resource(fooGVR).Namespace("default").Create(ctx, foo, metav1.CreateOptions{})
 		Expect(err).NotTo(HaveOccurred())
 
+		// Wait until deployment exists and has 1 ready replica
 		Eventually(func() bool {
 			dep, err := kubeClient.AppsV1().Deployments("default").Get(ctx, "test-foo", metav1.GetOptions{})
-			return err == nil && dep.Status.Replicas == 1
-		}, 15*time.Second, 500*time.Millisecond).Should(BeTrue())
+			return err == nil && dep.Status.ReadyReplicas == 1
+		}, 30*time.Second, 1*time.Second).Should(BeTrue())
+
+		// Cleanup
+		_ = dynClient.Resource(fooGVR).Namespace("default").Delete(ctx, "test-foo", metav1.DeleteOptions{})
 	})
 })
